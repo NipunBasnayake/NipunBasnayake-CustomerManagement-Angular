f7317e5c914490510ad6433604857a9f06cd4038
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-03-08 23:59:26 +0530

    Update Frontend

--- src/app/addcustomer/addcustomer.component.ts
+++ src/app/addcustomer/addcustomer.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-addcustomer',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './addcustomer.component.html',
   styleUrl: './addcustomer.component.css'
 })
 export class AddcustomerComponent {
-
+  public userName:string = "Saman";
 }
--- src/app/managecustomer/managecustomer.component.ts
+++ src/app/managecustomer/managecustomer.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-managecustomer',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './managecustomer.component.html',
   styleUrl: './managecustomer.component.css'
 })
 export class ManagecustomerComponent {
+  public customers:any = [];
+
+  constructor(private http:HttpClient){
+    this.loadCustomer();
+  }
+
+  loadCustomer(){
+    this.http.get("http://localhost:8080/customer/all").subscribe(data=>{
+      console.log(data);
+      this.customers = data;
+    });
+  }
+
+  // public customers = [
+  //   {
+  //     id: 1,
+  //     name: "Nipun Sathsara B",
+  //     address: "Gampaha",
+  //     phone: "0987654321",
+  //     email: "nipun@gmail.com"
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Nipun Sathsara",
+  //     address: "Gampaha",
+  //     phone: "0987654321",
+  //     email: "nipun@gmail.com"
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Nipun S",
+  //     address: "Gampaha",
+  //     phone: "0987654321",
+  //     email: "nipun@gmail.com"
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Nipun S",
+  //     address: "Gampaha",
+  //     phone: "0987654321",
+  //     email: "nipun@gmail.com"
+  //   }
+  // ]
 
 }
